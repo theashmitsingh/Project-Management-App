11f7f47f9a86c5065810564c17efc7cf6b9aac88
Ashmit Singh <theashmitsingh11@gmail.com>
2026-06-21 09:46:01 +0530

    Fixed the prisma file error

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { neonConfig } from '@neondatabase/serverless'
 
